a08625cbc1b8d7dbe4581c2205b5efaae216ff89
Nicolás Aranguren Turmequé <nicolasarangurenturmeque@Laptop-de-Nicolas.local>
2024-12-12 18:39:47 -0500

    Change main

--- app_1.py
+++ app_1.py
@@ -9,7 +9,7 @@ from io import StringIO
 import pandas as pd
 from joblib import load
 
-from .models import Prediction, Item, Base
+from . import models
 from datetime import datetime
 import pytz
 
